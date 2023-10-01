c2a343050559a16eaf62e266e411366996779266
bikmetle <hamid-nv@vivaldi.net>
2023-10-01 20:50:30 +0300

    tests

---
+++ api/v1/users/tests.py
@@ -0,0 +1,91 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
+class UserViewSetTestCase(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.user_1 = get_user_model().objects.create(id=1, username='one', password=1, is_staff=True)
+        cls.user_2 = get_user_model().objects.create(id=2, username='two', password=12)
+        cls.user_3 = get_user_model().objects.create(id=3, username='three', password=123)
+        cls.list_url = reverse("api:v1:users-list")
+        cls.detail_url = reverse("api:v1:users-detail", kwargs={"pk": cls.user_3.id})
+
+    def setUp(self):
+        self.client.force_login(self.user_1)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_list(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 3)
+
+    def test_create(self):
+        data = {
+            "username": "four",
+            "password": 1234
+        }
+        response = self.client.post(self.list_url, data=data, format="json")
+        self.assertEqual(response.status_code, 201)
+        self.assertIn(
+            response.json()["id"], 
+            get_user_model().objects.values_list("id", flat=True)
+        )
+
+    def test_retrieve(self):
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['id'], self.user_3.id)
+        self.assertEqual(response.json()['username'], self.user_3.username)
+
+    def test_update(self):
+        data = {
+            "username": self.user_3.username,
+            "password": self.user_3.password,
+            "first_name": "John",
+            "last_name": "Smith"
+        }
+
+        response = self.client.put(self.detail_url, data=data, format="json")
+        self.assertEqual(response.status_code, 200)
+
+        user = get_user_model().objects.filter(id=self.user_3.id).first()
+        self.assertEqual(user.first_name, data["first_name"])
+        self.assertEqual(user.last_name, data["last_name"])
+
+    def test_destroy(self):
+        response = self.client.delete(self.detail_url, format="json")
+        self.assertEqual(response.status_code, 204)
+        self.assertNotIn(
+            self.user_3.id, 
+            get_user_model().objects.values_list("id", flat=True)
+        )
+
+    def test_auth(self):
+        self.client.logout()
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.post(self.list_url)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.put(self.detail_url)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_urls(self):
+        self.assertEqual(self.list_url, "/api/v1/users")
+        self.assertEqual(
+            self.detail_url,
+            f"/api/v1/users/{self.user_3.id}"
+        )
+
--- api/v1/users/views.py
+++ api/v1/users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from api.v1.users.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
