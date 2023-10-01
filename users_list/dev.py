b96ffb285606ec9e797f1181cc6ec1a06e0fbca8
bikmetle <hamid-nv@vivaldi.net>
2023-10-01 14:05:39 +0300

    init

---
+++ users_list/dev.py
@@ -0,0 +1,7 @@
+from .settings import *
+
+SECRET_KEY = "whatever"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1"]
---
+++ users_list/prod.py
@@ -0,0 +1,7 @@
+from settings import *
+
+SECRET_KEY = "django-insecure-808!!n_3$aoo8k@80gk)6mx9-rk9ird89g0ha36))v)6z-!-&u"
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
