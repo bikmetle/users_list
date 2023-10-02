66ab453e35ab7f70530b3abcda6c65d2a5219b6f
bikmetle <hamid-nv@vivaldi.net>
2023-10-02 06:50:09 +0300

    add token auth

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path("v1/", include(("api.v1.urls", "v1"), namespace="v1")),
+    path('token/', ObtainAuthToken.as_view(), name='get_token'),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "swagger-ui/",
--- users_list/settings.py
+++ users_list/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "users",
     "rest_framework",
+    'rest_framework.authtoken',
     "drf_spectacular",
 ]
 
@@ -118,4 +119,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
